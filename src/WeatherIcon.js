0e869681dd1da76c94aa8caa90cb36a2827daee1
CynthiaLG <73857367+CynthiaLG@users.noreply.github.com>
2021-02-18 14:55:57 -0600

    Addes icons
    Added icons for every code from API

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,11 +2,29 @@ import React from "react";
 import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function WeatherIcon (props){
-    if 
-    return  <ReactAnimatedWeather
-    icon= "CLEAR_DAY"
+    const codeMapping = {
+        "01d": "CLEAR_DAY",
+        "01n": "CLEAR_NIGHT",
+        "02d":"PARTLY_CLOUDY_DAY",
+        "02n": "PARTLY_CLOUDY_NIGHT",
+        "04d":"CLOUDY",
+        "09d":"RAIN",
+        "09n":"RAIN",
+        "010d":"RAIN",
+        "010n":"RAIN",
+        "011d":"RAIN",
+        "011n":"RAIN",
+        "13d": "SNOW",
+        "13n": "SNOW",
+        "50d":"FOG",
+        "50n":"FOG",
+    };
+    return (
+         <ReactAnimatedWeather
+    icon= {codeMapping[props.code]}
     color= "#DB6400"
     size= {100}
     animate= {true}
-  />
+    />
+  );
 }
